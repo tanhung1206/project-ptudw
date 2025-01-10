2f92547fc0ef8de3e37ac6c6f15ec7351d7e548e
TAC <anh2017ntp@gmail.com>
2025-01-10 21:57:56 +0700

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.DB_USER,         // PostgreSQL user
-    host: process.env.DB_HOST,         // PostgreSQL host
-    database: process.env.DB_DATABASE, // PostgreSQL database name
-    password: process.env.DB_PASSWORD, // PostgreSQL password
-    port: process.env.DB_PORT,         // PostgreSQL port
-    // ssl: {
-    //     rejectUnauthorized: true
-    // }
+  user: process.env.DB_USER, // PostgreSQL user
+  host: process.env.DB_HOST, // PostgreSQL host
+  database: process.env.DB_DATABASE, // PostgreSQL database name
+  password: process.env.DB_PASSWORD, // PostgreSQL password
+  port: process.env.DB_PORT, // PostgreSQL port
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 module.exports = pool;
